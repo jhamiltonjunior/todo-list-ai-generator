d1c8d56ab51d6e681dd355cb9fd6405037668703
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2025-02-26 20:34:22 -0300

    firts commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+        <input className={'input input-bordered '} type="text" placeholder="Type something..." />
+    </main>
+  )
+}
+
+export default App
