3298169986c8f80d45f1542a8b556e932d69a2dd
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2025-02-26 21:37:34 -0300

    pesquisador em cima

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import {Brain} from "lucide-react";
 
 function App() {
 
   return (
-    <main>
-        <input className={'input input-bordered'} type="text" placeholder="Type something..." />
+    <main className={"border-gray-800 w-screen h-screen flex flex-col gap-3"}>
+        <div className={"bg-[#353866] text-white p-8 shadow flex flex-row items-center justify-center gap-3"}>
+            <input className={'input input-bordered py-1 w-full max-w-2xl'} type="text" placeholder="Type something..."/>
+            <button
+                className={'btn border-none hover:bg-[--primary-color-dark] bg-[--primary-color] tooltip tooltip-bottom'}
+
+                data-tip="Gerar TODO List"
+            >
+                <Brain color={"white"} />
+            </button>
+        </div>
+
+        <div className={"bg-gray-800 text-white p-4"}>
+            <h1 className={"text-4xl"}>Hello, World!</h1>
+            <p className={"text-lg"}>This is a React app with Tailwind CSS</p>
+        </div>
     </main>
   )
 }
